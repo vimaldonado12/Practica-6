500cf5a61e57e26259a98e96a61947bd67ff0771
vimaldonado12 <vivian.maldonado4y@gmail.com>
2026-03-19 14:22:19 -0600

    ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  short enteroNumero1 = 115;
+  signed int enteroNumero2 = 55;
+  unsigned long enteroNumero3 = 789;
+  char caracterA = 65;
+  char caracterB = 'B';
+       
+  float puntoFlotanteNumero1 = 89.8;
+
+  unsigned double puntoFlotanteNumero2 = 238.2236;
+  return 0; 
+}
