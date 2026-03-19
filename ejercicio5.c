fb1b96358b390caf114b20957df400bfccc2f7cf
vimaldonado12 <vivian.maldonado4y@gmail.com>
2026-03-19 14:43:03 -0600

    ejercicio5.c

--- ejercicio5.c
+++ ejercicio5.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+int main() 
+}
+  int enteroNumero;
+  char caracterA = 65;  // Convierte el entero a carácter ASCII.  
+  double pountoFlotanteNumero;
+  
+  // Asignar valor de teclado a una variable.
+  printf("Escriba un valor entero: ");
+  scanf("%i", &enteroNumero);
+  printf("Escriba un valor real: ");
+  scanf("%lf", &puntoFlotanteNumero);
+
+  // Imprimir valores con formato.
+  printf("\nImprimiendo las variables \a\n");
+  printf("\t Valor de enteroNumero = %i \a\n", enteroNumero);
+  printf("\t Valor de caracterA = %c \a\n", caracterA);
+  printf("\t Valor de puntoFlotanteNumero = %lf \a\n",
+       puntoFlotanteNumero);
+
+  printf("\t Valor de enteroNumero en base 16 = %x \a\n", enteroNumero);
+  printf("\t Valor de caracterA en código hexadecimal = %x\n", caracterA);
+  printf("\t Valor de puntoFlotanteNumero\n");
+  printf("en notación científica = %e\n", puntoFlotanteNumero);
+
+  return 0;
+}
