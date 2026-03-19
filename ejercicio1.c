2ac70153d8cb568e1b9a68a41acfbbc66e4671cc
vimaldonado12 <vivian.maldonado4y@gmail.com>
2026-03-19 14:16:54 -0600

    ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+// Comentario por línea
+/* Comentario por bloque
+    que puede ocupar 
+    varios renglones */
+    // Este código compila y ejecuta 
+    /* pero no muestra salida alguna 
+    debido a que un comentario
+    ya sea por línea o por bloque */
+// no es tomado en cuenta al momento
+// de compilar el programa,
+/* sólo sirve como documentación en el */ 
+/*
+    código fuente 
+*/
+return 0;
+}
