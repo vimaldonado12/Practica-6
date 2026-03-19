ba0361f2afa3f153dcf3b179a54055868afe8e5e
vimaldonado12 <vivian.maldonado4y@gmail.com>
2026-03-19 14:35:22 -0600

    ejercicio4.c

---
+++ ejercicio4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+
+  int entero;
+  float flotante;
+
+  printf("Ingresa el valor entero: ");
+  scanf("%i", &entero);
+  printf("El valor ingresado es: %d\n", entero);
+
+  printf("Ingresa el valor float: ");
+  scanf("%f", &flotante);
+  printf("El valor ingresado es: %f\n", flotante);
+
+  return 0;
+}
